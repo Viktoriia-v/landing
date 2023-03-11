21379cb5a00157c68a9b0a532b5ab6397cf82152
Viktoriia-v <123397343+Viktoriia-v@users.noreply.github.com>
2023-03-11 18:37:58 +0000

    C

---
+++ main.js
@@ -0,0 +1,52 @@
+//1st task
+console.log('Kucher');
+
+//2nd task
+const x = 3;
+const y = 15;
+console.log(x);
+console.log(y);
+
+//3rd task
+const a = x;
+console.log(a);
+
+//4th task
+const string = "Hello";
+console.log(string);
+const num = 10;
+console.log(num);
+const is_true = true;
+console.log(is_true);
+var b;
+console.log(b);
+var c = null;
+console.log(c);
+
+//5th task
+var adult = confirm("Ви досягли повнолітнього віку?");
+console.log(adult);
+
+//6 task
+const firstName = "Vika";
+const lastName = "Kucher";
+const year = 2005;
+const married = false;
+
+console.log(year, typeof year);
+console.log(married, typeof married);
+console.log(firstName + " " + lastName + ". ", typeof (firstName + " " + lastName + ". "));
+
+const variable = null;
+let d;
+console.log(variable, typeof variable);
+console.log(d, typeof d);
+
+//7 task
+let login = prompt('Введіть ваш login', '');
+
+let email = prompt('Введіть ваш email', '');
+
+let password = prompt('Введіть ваш password', '');
+
+console.log('Dear User, your login',login, ',email is',email, ', your password is',password);
